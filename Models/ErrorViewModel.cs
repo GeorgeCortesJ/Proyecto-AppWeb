e6e78d0cfc9c6eee45e9be9bae4a3ff3e02b2157
D1ec18ch8 <153028967+D1ec18ch8@users.noreply.github.com>
2025-05-04 16:31:25 -0600

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBeachSA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
