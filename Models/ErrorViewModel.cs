15740872572467bc4a02d8c149fba25f2858b99b
D1ec18ch8 <153028967+D1ec18ch8@users.noreply.github.com>
2025-05-04 16:24:37 -0600

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Prueba_Proyecto.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
